b6b39fbd197f06949e670bdc18238042fcab9b71
Mehedi Hasan Antor <antordon500@gmail.com>
2024-02-19 21:29:07 +0600

    Finished Program

--- demo/src/main/java/Assignment9.java
+++ demo/src/main/java/Assignment9.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
 public class Assignment9 {
-    
+    public static void main(String[] args){
+        System.out.println("CALL CENTER");
+        Scanner input = new Scanner(System.in);
+        int opt;
+        System.out.println("Insert option between (1-3)");
+        opt =input.nextInt();
+        switch (opt) {
+            case 1:
+                System.out.println("Selected language is Bengali.");
+                break;
+            case 2:
+                System.out.println("Selected language is Hindi.");
+                break;
+            case 3:
+                System.out.println("Selected language is Urdu.");
+                break;
+            default:
+                System.out.println("Selected language is English.");
+                break;
+        }
+    }
 }
